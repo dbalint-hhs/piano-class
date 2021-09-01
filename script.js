9df0346436318bf9012d58253387dfabbfb92e43
dbalint-hhs <89852908+dbalint-hhs@users.noreply.github.com>
2021-09-01 14:19:13 -0400

    Added functionality for button/list 4

--- script.js
+++ script.js
@@ -3,11 +3,13 @@
 let button1 = document.getElementById("level-1-button");
 let button2 = document.getElementById("level-2-button");
 let button3 = document.getElementById("level-3-button");
+let button4 = document.getElementById("level-4-button");
 
 let splash = document.getElementById("splash");
 let list1 = document.getElementById("level-1-list");
 let list2 = document.getElementById("level-2-list");
 let list3 = document.getElementById("level-3-list");
+let list4 = document.getElementById("level-4-list");
 
 // When the Level 1 button is clicked, hide splash and any other lists that might be open.
 
@@ -17,6 +19,7 @@ button1.addEventListener("click", function() {
   list1.style.display = 'block';
   list2.style.display = 'none';
   list3.style.display = 'none';
+  list4.style.display = 'none';
 });
                        
 button2.addEventListener("click", function() {
@@ -25,6 +28,7 @@ button2.addEventListener("click", function() {
   list1.style.display = 'none';
   list2.style.display = 'block';
   list3.style.display = 'none';
+  list4.style.display = 'none';
 });
 
 button3.addEventListener("click", function() {
@@ -33,4 +37,14 @@ button3.addEventListener("click", function() {
   list1.style.display = 'none';
   list2.style.display = 'none';
   list3.style.display = 'block';
+  list4.style.display = 'none';
+});
+
+button4.addEventListener("click", function() {
+  console.log("Level 4 clicked!");
+  splash.style.display = "none";
+  list1.style.display = 'none';
+  list2.style.display = 'none';
+  list3.style.display = 'none';
+  list4.style.display = 'block';
 });
