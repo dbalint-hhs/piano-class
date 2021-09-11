c32100091ad3f122a580eebb925e61ddf9a76218
dbalint-hhs <89852908+dbalint-hhs@users.noreply.github.com>
2021-09-11 11:09:47 -0400

    added level-5-list and button functionality

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ let button1 = document.getElementById("level-1-button");
 let button2 = document.getElementById("level-2-button");
 let button3 = document.getElementById("level-3-button");
 let button4 = document.getElementById("level-4-button");
+let button5 = document.getElementById("level-5-button");
 
 let splash = document.getElementById("splash");
 let list1 = document.getElementById("level-1-list");
 let list2 = document.getElementById("level-2-list");
 let list3 = document.getElementById("level-3-list");
 let list4 = document.getElementById("level-4-list");
+let list5 = document.getElementById("level-5-list");
 
 // When the Level 1 button is clicked, hide splash and any other lists that might be open.
 
@@ -20,8 +22,11 @@ button1.addEventListener("click", function() {
   list2.style.display = 'none';
   list3.style.display = 'none';
   list4.style.display = 'none';
+  list5.style.display = 'none';
 });
-                       
+
+// When the Level 2 button is clicked, hide splash and any other lists that might be open.
+
 button2.addEventListener("click", function() {
   console.log("Level 2 clicked!");
   splash.style.display = "none";
@@ -29,7 +34,10 @@ button2.addEventListener("click", function() {
   list2.style.display = 'block';
   list3.style.display = 'none';
   list4.style.display = 'none';
+  list5.style.display = 'none';
 });
+
+// When the Level 3 button is clicked, hide splash and any other lists that might be open.
 
 button3.addEventListener("click", function() {
   console.log("Level 3 clicked!");
@@ -38,7 +46,11 @@ button3.addEventListener("click", function() {
   list2.style.display = 'none';
   list3.style.display = 'block';
   list4.style.display = 'none';
+  list5.style.display = 'none';
 });
+
+
+// When the Level 4 button is clicked, hide splash and any other lists that might be open.
 
 button4.addEventListener("click", function() {
   console.log("Level 4 clicked!");
@@ -47,4 +59,18 @@ button4.addEventListener("click", function() {
   list2.style.display = 'none';
   list3.style.display = 'none';
   list4.style.display = 'block';
+  list5.style.display = 'none';
+});
+
+
+// When the Level 5 button is clicked, hide splash and any other lists that might be open.
+
+button5.addEventListener("click", function() {
+  console.log("Level 5 clicked!");
+  splash.style.display = "none";
+  list1.style.display = 'none';
+  list2.style.display = 'none';
+  list3.style.display = 'none';
+  list4.style.display = 'none';
+  list5.style.display = 'block';
 });
