76b8a5faa107917e096d900500db743c7e1da131
dbalint-hhs <89852908+dbalint-hhs@users.noreply.github.com>
2021-11-18 18:21:26 -0500

    Automated the js script with for loops
    So it isn't explicitly targeting every button and list.

--- script.js
+++ script.js
@@ -1,144 +1,18 @@
 // Find the buttons & the lists
 
-let button1 = document.getElementById("level-1-button");
-let button2 = document.getElementById("level-2-button");
-let button3 = document.getElementById("level-3-button");
-let button4 = document.getElementById("level-4-button");
-let button5 = document.getElementById("level-5-button");
-let button6 = document.getElementById("level-6-button");
-let button7 = document.getElementById("level-7-button");
-let button8 = document.getElementById("level-8-button");
+let buttons = document.getElementsByClassName('level-button');
+console.log(buttons);
+let lists = document.getElementsByClassName('level-list');
+console.log(lists);
 
-let splash = document.getElementById("splash");
-let list1 = document.getElementById("level-1-list");
-let list2 = document.getElementById("level-2-list");
-let list3 = document.getElementById("level-3-list");
-let list4 = document.getElementById("level-4-list");
-let list5 = document.getElementById("level-5-list");
-let list6 = document.getElementById("level-6-list");
-let list7 = document.getElementById("level-7-list");
-let list8 = document.getElementById("level-8-list");
+let listLoader = function(i) {
+  for (let j = 0; j < lists.length; j++) {
+    lists[j].setAttribute("display", "hidden");
+  }
+  list[i].setAttribute("display", "block");
+}
 
-// When the Level 1 button is clicked, hide splash and any other lists that might be open.
-
-button1.addEventListener("click", function() {
-  console.log("Level 1 clicked!");
-  splash.style.display = "none";
-  list1.style.display = 'block';
-  list2.style.display = 'none';
-  list3.style.display = 'none';
-  list4.style.display = 'none';
-  list5.style.display = 'none';
-  list6.style.display = 'none';
-  list7.style.display = 'none';
-  list8.style.display = 'none';
-});
-
-// When the Level 2 button is clicked, hide splash and any other lists that might be open.
-
-button2.addEventListener("click", function() {
-  console.log("Level 2 clicked!");
-  splash.style.display = "none";
-  list1.style.display = 'none';
-  list2.style.display = 'block';
-  list3.style.display = 'none';
-  list4.style.display = 'none';
-  list5.style.display = 'none';
-  list6.style.display = 'none';
-  list7.style.display = 'none';
-  list8.style.display = 'none';
-});
-
-// When the Level 3 button is clicked, hide splash and any other lists that might be open.
-
-button3.addEventListener("click", function() {
-  console.log("Level 3 clicked!");
-  splash.style.display = "none";
-  list1.style.display = 'none';
-  list2.style.display = 'none';
-  list3.style.display = 'block';
-  list4.style.display = 'none';
-  list5.style.display = 'none';
-  list6.style.display = 'none';
-  list7.style.display = 'none';
-  list8.style.display = 'none';
-});
-
-
-// When the Level 4 button is clicked, hide splash and any other lists that might be open.
-
-button4.addEventListener("click", function() {
-  console.log("Level 4 clicked!");
-  splash.style.display = "none";
-  list1.style.display = 'none';
-  list2.style.display = 'none';
-  list3.style.display = 'none';
-  list4.style.display = 'block';
-  list5.style.display = 'none';
-  list6.style.display = 'none';
-  list7.style.display = 'none';
-  list8.style.display = 'none';
-});
-
-
-// When the Level 5 button is clicked, hide splash and any other lists that might be open.
-
-button5.addEventListener("click", function() {
-  console.log("Level 5 clicked!");
-  splash.style.display = "none";
-  list1.style.display = 'none';
-  list2.style.display = 'none';
-  list3.style.display = 'none';
-  list4.style.display = 'none';
-  list5.style.display = 'block';
-  list6.style.display = 'none';
-  list7.style.display = 'none';
-  list8.style.display = 'none';
-});
-
-
-// When the Level 6 button is clicked, hide splash and any other lists that might be open.
-
-button6.addEventListener("click", function() {
-  console.log("Level 6 clicked!");
-  splash.style.display = "none";
-  list1.style.display = 'none';
-  list2.style.display = 'none';
-  list3.style.display = 'none';
-  list4.style.display = 'none';
-  list5.style.display = 'none';
-  list6.style.display = 'block';
-  list7.style.display = 'none';
-  list8.style.display = 'none';
-});
-
-// When the Level 7 button is clicked, hide splash and any other lists that might be open.
-
-button7.addEventListener("click", function() {
-  console.log("Level 7 clicked!");
-  splash.style.display = "none";
-  list1.style.display = 'none';
-  list2.style.display = 'none';
-  list3.style.display = 'none';
-  list4.style.display = 'none';
-  list5.style.display = 'none';
-  list6.style.display = 'none';
-  list7.style.display = 'block';
-  list8.style.display = 'none';
-});
-
-// When the Level 8 button is clicked, hide splash and any other lists that might be open.
-
-button8.addEventListener("click", function() {
-  console.log("Level 8 clicked!");
-  splash.style.display = "none";
-  list1.style.display = 'none';
-  list2.style.display = 'none';
-  list3.style.display = 'none';
-  list4.style.display = 'none';
-  list5.style.display = 'none';
-  list6.style.display = 'none';
-  list7.style.display = 'none';
-  list8.style.display = 'block';
-});
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', listLoader(i));
+}
 
