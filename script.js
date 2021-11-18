222289feed6452fdf06d3a8742483d955a9435e4
dbalint-hhs <89852908+dbalint-hhs@users.noreply.github.com>
2021-11-18 18:43:29 -0500

    missing semi colon??

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ console.log(lists);
 let splash = document.getElementById('splash');
 
 let listLoader = function() {
-  console.log("Click detected!")
+  console.log("Click detected!");
   splash.setAttribute("display", "none");
    /* for (let j = 0; j < lists.length; j++) {
     lists[j].setAttribute("display", "hidden");
