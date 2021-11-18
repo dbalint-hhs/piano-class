a5441c848fee9ae75bc0f00d1ae8b0e5ec18e14a
dbalint-hhs <89852908+dbalint-hhs@users.noreply.github.com>
2021-11-18 18:38:22 -0500

    comments for testing

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ let splash = document.getElementById('splash');
 
 let listLoader = function() {
   splash.setAttribute("display", "hidden");
-   for (let j = 0; j < lists.length; j++) {
+   /* for (let j = 0; j < lists.length; j++) {
     lists[j].setAttribute("display", "hidden");
   }
   let currentList = document.getElementById("level-" + this.id + "-list");
   console.log(currentList);
-  currentList.setAttribute("display", "block");
+  currentList.setAttribute("display", "block"); */
 }
 
 for (let i = 0; i < buttons.length; i++) {
