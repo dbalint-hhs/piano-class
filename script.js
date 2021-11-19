b0212b04ed40f10acf19631fd5c6a66972bd5925
dbalint-hhs <89852908+dbalint-hhs@users.noreply.github.com>
2021-11-19 10:11:51 -0500

    cut unnecessary "splash" code

--- script.js
+++ script.js
@@ -2,11 +2,9 @@
 
 let buttons = document.getElementsByClassName('level-button');
 let lists = document.getElementsByClassName('level-list');
-let splash = document.getElementById('splash');
 
 let listLoader = function() {
   console.log("Click detected!" + this.id);
-  splash.style.display = "none";
   for (let i = 0; i < lists.length; i++) {
     lists[i].style.display = "none";
   };
