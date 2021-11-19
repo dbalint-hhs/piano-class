2d36e575519943567378962be03eb524e3c84dba
dbalint-hhs <89852908+dbalint-hhs@users.noreply.github.com>
2021-11-18 19:02:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
 // Find the buttons & the lists
 
 let buttons = document.getElementsByClassName('level-button');
-console.log(buttons);
 let lists = document.getElementsByClassName('level-list');
-console.log(lists);
 let splash = document.getElementById('splash');
 
 let listLoader = function() {
-  console.log("Click detected!" + this.id);
-  splash.style.display = "none";
-  /*
-  for (let j = 0; j < lists.length; j++) {
-    lists[j].style.display = "none";
-  }
-  
-  let currentList = document.getElementById("level-" + i + "-list");
-  console.log(currentList);
-  currentList.style.display = "block"; */
+  console.log("Click detected!");
 }
 
 for (let i = 0; i < buttons.length; i++) {
