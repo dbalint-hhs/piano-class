bd331b84ae32bfe052542a5f85281e2d5113a83e
DBalint88 <dbalint88@gmail.com>
2022-09-17 19:03:40 -0400

    Fully implement sliding secondary menus

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
-let listOfLevels = document.getElementById("list-of-levels")
-let levelOneSongs = document.getElementById("level-1-songs")
+let levelList = document.getElementById("level-list");
+let levelButton = document.getElementsByClassName("level-button");
+let songList = document.getElementsByClassName("song-list");
+let backButton = document.getElementById("back-button");
 
-listOfLevels.addEventListener("click", () => {
-  listOfLevels.classList.add("inactive")
-  levelOneSongs.classList.add("active")
-})
+
+for (let i = 0; i < levelButton.length; i++) {
+  levelButton[i].addEventListener("click", callSongList);
+}
+
+backButton.addEventListener("click", hideSongList);
+
+function callSongList(e) {
+  levelList.classList.add("inactive-level-list");
+  for (let j = 0; j < songList.length; j++) {
+    console.log(this.id)
+    if (songList[j].id == ("level-" + (this.id))) {
+      songList[j].classList.add("active-song-list");
+    }
+  }
+  backButton.classList.add("back-button-active")
+
+    
+}
+
+function hideSongList() {
+  for (let k = 0; k < songList.length; k++) {
+    songList[k].classList.remove("active-song-list") 
+  }
+  levelList.classList.remove("inactive-level-list");
+  backButton.classList.remove("back-button-active");
+}
+
