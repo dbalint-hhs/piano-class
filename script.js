3ec7fc994ab8d02aa0997380f927a669f3812ff4
DBalint88 <dbalint88@gmail.com>
2022-09-17 17:29:36 -0400

    Swap out accordion nav for a sliding secondary menu

--- script.js
+++ script.js
@@ -1,37 +1,7 @@
-let accTitle = document.getElementsByClassName("acc-heading");
-let accContent = document.getElementsByClassName("acc-content");
+let listOfLevels = document.getElementById("list-of-levels")
+let levelOneSongs = document.getElementById("level-1-songs")
 
-for (let j = 0; j < accContent.length; j++) {
-  let realHeight = accContent[j].offsetHeight;
-  accContent[j].setAttribute("data-height", realHeight + "px");
-  accContent[j].style.height = 0;
-}
-
-for (let i = 0; i < accTitle.length; i++) {
-  accTitle[i].onclick = function () {
-    let openedAcc = this.getAttribute('href').replace('#','')
-
-    if (this.classList.contains("active")) {
-      this.classList.remove("active");
-      document.getElementsById(openedAcc).style.height = 0;
-
-      return false;
-    } 
-    for (let k = 0; k < accTitle.length; k++) {
-      accTitle[k].classList.remove("active");
-    }
-    for (let j = 0; j < accContent.length; j++) {
-      accContent[j].style.height = 0;
-      
-    }
-
-    this.classList.add("active");
-
-    document.getElementById(openedAcc).style.height = accContent[i].getAttribute("data-height");
-
-    return false;
-
-
-  }
-  
-}
+listOfLevels.addEventListener("click", () => {
+  listOfLevels.classList.add("inactive")
+  levelOneSongs.classList.add("active")
+})
