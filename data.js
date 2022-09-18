3014896c6b87ab8f35b29a60754256c8b3ea4064
DBalint88 <dbalint88@gmail.com>
2022-09-17 20:40:54 -0400

    Prove one working  example of lazy-loaded pdf and dynamic video link

---
+++ data.js
@@ -0,0 +1,11 @@
+const songLists = {
+    'Level 1'  : {
+        'Hot Cross Buns' : {
+            pdf : "images/01-hot-cross-buns.pdf",
+            video : "https://youtu.be/PaaAvBGLem0"
+        },
+        "Mary Had A Little Lam" : {
+
+        }
+    }
+}
--- script.js
+++ script.js
@@ -1,27 +1,42 @@
+// Variables
+
 let levelList = document.getElementById("level-list");
 let levelButton = document.getElementsByClassName("level-button");
 let songList = document.getElementsByClassName("song-list");
+let songButton = document.getElementsByClassName("song-button")
 let backButton = document.getElementById("back-button");
+let iframe = document.getElementById("iframe");
+let splash = document.getElementById("splash");
+let videoLink = document.getElementById("video-link");
 
+
+
+// Click Events
+
+backButton.addEventListener("click", hideSongList);
 
 for (let i = 0; i < levelButton.length; i++) {
   levelButton[i].addEventListener("click", callSongList);
 }
 
-backButton.addEventListener("click", hideSongList);
+for (let l = 0; l < songButton.length; l++) {
+  songButton[l].addEventListener("click", loadSong);
+}
+
+
+
+// Functions
 
 function callSongList(e) {
   levelList.classList.add("inactive-level-list");
   for (let j = 0; j < songList.length; j++) {
-    console.log(this.id)
     if (songList[j].id == ("level-" + (this.id))) {
       songList[j].classList.add("active-song-list");
     }
   }
-  backButton.classList.add("back-button-active")
-
-    
+  backButton.classList.add("back-button-active")    
 }
+
 
 function hideSongList() {
   for (let k = 0; k < songList.length; k++) {
@@ -31,3 +46,11 @@ function hideSongList() {
   backButton.classList.remove("back-button-active");
 }
 
+
+function loadSong(e) {
+  splash.style.display = "none";
+  iframe.style.width = "100%";
+  iframe.style.height = "100%";
+  iframe.src = this.dataset.pdf + "#zoom=118";
+  videoLink.href = this.dataset.video;
+}
